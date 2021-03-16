d49c0328a2c2bf2f672c729c043b77118d55d9bb
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-16 10:51:21 -0500

    board construction method

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,6 +1,22 @@
+#construct board
+def render_board
+  
+  def render_row
+    $row_break = ["-----------------------------------------"]
+    puts $row_break
+    8.times { print "|    "}
+    puts "|"
+  end
+
+  8.times { render_row }
+  puts $row_break
+end  
+
+render_board
+
 def knight_moves (vertex_1, vertex_2)
 
-  #construct board
+
   #construct undirected graph representing every possible move of a knight on a board
   #take first parameter (vertex) and locate in the graph
   #take second parameter (vertex) and locate in the graph
@@ -8,3 +24,4 @@ def knight_moves (vertex_1, vertex_2)
   #display each vertex from first location to second location
 
 end  
+
