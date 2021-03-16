ecea7e10ef53e18c03e64cb2ec158d08098fc17f
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-16 10:14:28 -0500

    pseudocode

---
+++ knights_travails.rb
@@ -0,0 +1,10 @@
+def knight_moves (vertex_1, vertex_2)
+
+  #construct board
+  #construct undirected graph representing every possible move of a knight on a board
+  #take first parameter (vertex) and locate in the graph
+  #take second parameter (vertex) and locate in the graph
+  #locate path with fewest edges between vertices
+  #display each vertex from first location to second location
+
+end  
