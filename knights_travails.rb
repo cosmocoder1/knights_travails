47b88c165736428687f4c192340ca094eb5746ed
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-16 20:02:33 -0500

    Node class, graph creator function dev

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,27 +1,66 @@
 #construct board
 def render_board
+
+  =begin  
+    def render_row
+      $row_break = ["-----------------------------------------"]
+      puts $row_break
+      8.times { print "|    "}
+      puts "|"
+    end
   
-  def render_row
-    $row_break = ["-----------------------------------------"]
+    8.times { render_row }
     puts $row_break
-    8.times { print "|    "}
-    puts "|"
+  =end
+  
+  end  
+  
+  render_board
+  
+  matrix = [
+  
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  
+  ]
+  
+  class Node 
+    attr_accessor :data, :adjacent
+  
+    def initialize (data)
+      @data = data
+      @adjacent = []
+    end
+    
+    def add_adjacent (node)
+      @adjacent.push(node)
+    end  
+  
   end
-
-  8.times { render_row }
-  puts $row_break
-end  
-
-render_board
-
-def knight_moves (vertex_1, vertex_2)
-
-
-  #construct undirected graph representing every possible move of a knight on a board
-  #take first parameter (vertex) and locate in the graph
-  #take second parameter (vertex) and locate in the graph
-  #locate path with fewest edges between vertices
-  #display each vertex from first location to second location
-
-end  
-
+  
+  def map_board (matrix)
+  
+    puts matrix.map.with_index { |row, j| row.map.with_index { |value, i| value = Node.new("#{j}, #{i})").data } }
+  
+  end
+  
+  map_board(matrix)
+  
+  
+  def knight_moves (vertex_1, vertex_2)
+  
+    #construct undirected graph representing every possible move of a knight on the board
+    #take first parameter (vertex) and locate in the graph
+    #take second parameter (vertex) and locate in the graph
+    #locate path with fewest edges between vertices
+    #display each vertex from first location to second location
+  
+  end  
+  
+  
