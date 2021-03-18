67fd3c1aef9acf64fc7b4f03b5c8a0ba2bd3d195
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-18 11:08:34 -0500

    adjacency map method bug fix complete

--- knights_travails.rb
+++ knights_travails.rb
@@ -51,26 +51,24 @@ def render_board
     #assign vertices to board spaces
     populated_matrix = matrix.map.with_index { |row, j|  row.map.with_index { |value, i| value = Node.new("#{i}, #{j}") } }.each_slice(8).map { |i| i }
   
-    puts populated_matrix.inspect
-  
     #assign adjacent
     board_adjacencies = populated_matrix[0].map.with_index { |row, i| row.map.with_index { |value, j| 
     puts "top"
     puts value.data
   
-      if populated_matrix[0][i + 2][j + 1] && (i + 2) < populated_matrix[0].length && (j + 1) < 8
+      if populated_matrix[0][i + 2] != nil && (i + 2) < populated_matrix[0].length && (j + 1) < 8
         puts "first block"
         value.adjacent.push(populated_matrix[0][i + 2][j + 1]) 
       end
-      if populated_matrix[0][i + 1][j + 2] && (i + 1) < populated_matrix[0].length && (j + 2) < 8
+      if populated_matrix[0][i + 1] != nil && (i + 1) < populated_matrix[0].length && (j + 2) < 8
         puts "second block"
         value.adjacent.push(populated_matrix[0][i + 1][j + 2]) 
       end
-      if populated_matrix[0][i + 2][j - 1] && (i + 2) < 8 && (j - 1) > -1
+      if populated_matrix[0][i + 2] != nil && (i + 2) < 8 && (j - 1) > -1
         puts "third block"
         value.adjacent.push(populated_matrix[0][i + 2][j - 1]) 
       end
-      if populated_matrix[0][i + 1][j - 2] && (i + 1) < 8 && (j - 2) > -1
+      if populated_matrix[0][i + 1] != nil && (i + 1) < 8 && (j - 2) > -1
         puts "fourth block"
         value.adjacent.push(populated_matrix[0][i + 1][j - 2]) 
       end
