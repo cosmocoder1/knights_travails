68ddb3cfab7b79acff27ded64593f54ff1516e0a
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-24 11:14:12 -0500

    graph search dev - refactoring

--- knights_travails.rb
+++ knights_travails.rb
@@ -80,14 +80,18 @@ def map_board
 end  
 
 class Knight 
-  attr_accessor :location, :stack, :checked, :path
+  attr_accessor :location, :checked, :parent
 
-  @location = []
-  @stack = []
-  @checked = []
-  @path = []
+  def initialize
+
+    @location = []
+    @checked = []
+    @parent = nil
+
+  end  
 
 end  
+
 
 def knight_moves 
 
@@ -108,8 +112,8 @@ def knight_moves
 
   x_start = 0
   y_start = 0
-  x_end = 7
-  y_end = 7
+  x_end = 4
+  y_end = 0
 
   board = map_board
 
@@ -121,23 +125,30 @@ def knight_moves
 
   def traverse (node_1, node_2)
 
+    queue = []
     target = node_2
     knight = Knight.new
     knight.location = node_1
-    node_1.adjacent.map { |vertex| knight.stack.push(vertex) }
-    i = 0
-    j = 0
- 
-    while knight.stack.length > 0 do
-    
-      if knight.stack[0] === node_2
-        knight.path.push(node_2) 
-      else
-        knight.checked.push(knight.stack[0])
-      end  
-    end  
- 
 
+    while knight.location != target do
+      knight.location.adjacent.map { |node| queue.push(node) }
+      knight.location = queue.shift
+    end
+
+    def trace_path (node)
+
+      count = 0
+      path = []
+      until node.parent === nil
+      node = node.parent
+      end
+      path
+      
+    end  
+      
+    
+    
+ 
   end  
 
 
@@ -148,4 +159,41 @@ end
 
 
 knight_moves 
+
+
+
+
+=begin
+
+while knight.queue.length > 0 do
+
+    puts "loop"
+      knight.queue.map { |value| if value != "level marker" 
+      puts value.data 
+    end}
+
+      if knight.queue[0] === "level marker"
+        puts "block 1"
+        count += 1
+        knight.queue.shift
+      elsif knight.queue[0] === target
+        puts "block 2"
+        knight.path.push(node_2.data)
+        knight.path.map { |space| 
+          puts space }   
+        return  
+      else
+        puts "block 3"
+        #knight.queue[0].parent = knight.location
+        knight.queue[0].adjacent.map { |vertex| if !knight.checked.include?(vertex) 
+        knight.queue.push(vertex) 
+      end}
+        knight.queue.shift
+        knight.location = knight.queue[0]
+      end  
+
+    end  
+
+=end
+
 
