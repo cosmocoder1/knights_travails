022cb5ecb4533798ea7056863d912b2c78cb3885
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-24 19:55:14 -0500

    refactoring, debug

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,39 +1,26 @@
 class Node 
-  attr_accessor :data, :adjacent
+  attr_accessor :data, :adjacent, :location, :previous
 
-  transformations = [ [1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [-2, -1], [2, -1] ]
+  TRANSFORMATIONS = [ [1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [-2, -1], [2, -1] ]
 
-  def initialize (position, previous)
-    @position = position
+  def initialize (location, previous)
+    @location = location
     @adjacent = []
     @checked = []
     @previous = nil
   end
+
+  def adjacent
+    TRANSFORMATIONS.map { |element| [element[0] + @location[0], element[1] + @location[1]] }
+    .map { |element| element.delete_if { |element| element[0] > 7 || element[0] < 1 || element[1] > 7 || element[1] < 1 } }
+    .map { |element| element.delete_if { |element| @checked.include?(element) } }.map { |element| @adjacent.push(Node.new(element, self)) }
+  end  
+
   
 end
 
-  def adjacent
-    transformations.map { |node| node[0] + position[0], node[1] + position[1] }
-    .map { |node| node.delete_if { |node| node[0] > 7 || node[0] < 1 || node[1] > 7 || node[1] < 1 } }
-    .map { |node| node.delete_if { |node| @checked.include?(node) } }
-  end  
 
-
-class Knight 
-  attr_accessor :location, :checked, :parent
-
-  def initialize
-
-    @location = []
-    @checked = []
-    @parent = nil
-
-  end  
-
-end  
-
-
-def knight_moves 
+def knight_moves (node_1, node_2)
 
 #retrieval script  
 =begin
@@ -50,59 +37,33 @@ def knight_moves
   x_end = 7 - gets.chomp.to_i
 =end
 
-  x_start = 0
-  y_start = 0
-  x_end = 7
-  y_end = 7
+  queue = []
+  target = node_2
+  knight = Node.new(node_1, nil)
 
-  board = map_board
-
-  #take first parameter (vertex) and locate in the graph
-  vertex_1 = board[0][y_start][x_start]
-  #take second parameter (vertex) and locate in the graph
-  vertex_2 = board[0][y_end][x_end]
-  #locate path with fewest edges between vertices
-
-  def traverse (node_1, node_2)
-
-    queue = []
-    target = node_2
-    knight = Knight.new
-    knight.location = node_1
-
-    while knight.location != target do
-      knight.location.adjacent.map { |node| unless queue.include?(node) 
-      queue.push(node) 
-      end}
-      knight.checked.push(queue[0])
-      knight.location = queue.shift
-    end
-
-    return knight.checked.map { |value| value.data }
-
-  end  
-
-=begin  
+  while knight.location != target do
+    knight.adjacent.map { |value| queue.push(value) }
+    knight.location = queue[0]
+    queue.shift
+  end
+ 
   def trace_path (node)
 
     count = 0
     path = []
-    until node.parent === nil
-    node = node.parent
+    until node.previous === nil
+    node = node.previous
     count += 1
     path.push(node)
     end
 
     return "you made it in #{count} moves - your path is #{path}"
       
-  end  
-=end    
+  end    
 
-  puts traverse(vertex_1, vertex_2)
-  #display each vertex from first location to second location
-
-end  
+end
 
 
-knight_moves 
+knight_moves([0, 0], [7, 7])
+
 
