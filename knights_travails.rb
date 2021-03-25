28c235109caf7330d309fe1d9aa38aa80730f76a
Nathan Lucy <cosmocoder1@gmail.com>
2021-03-24 19:10:51 -0500

    board simplify, refactoring

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,83 +1,23 @@
-#construct board
-def render_board
-
-end  
-
-render_board
-
-
 class Node 
   attr_accessor :data, :adjacent
 
-  def initialize (data)
-    @data = data
+  transformations = [ [1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [-2, -1], [2, -1] ]
+
+  def initialize (position, previous)
+    @position = position
     @adjacent = []
+    @checked = []
+    @previous = nil
   end
   
-  def add_adjacent (node)
-    @adjacent.push(node)
-  end  
-
 end
 
-def map_board
+  def adjacent
+    transformations.map { |node| node[0] + position[0], node[1] + position[1] }
+    .map { |node| node.delete_if { |node| node[0] > 7 || node[0] < 1 || node[1] > 7 || node[1] < 1 } }
+    .map { |node| node.delete_if { |node| @checked.include?(node) } }
+  end  
 
-  matrix = [
-
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-
-]
-
-  #assign vertices to board spaces
-  populated_matrix = matrix.map.with_index { |row, j|  row.map.with_index { |value, i| value = Node.new("#{i}, #{j}") } }.each_slice(8).map { |i| i }
-
-  #assign adjacencies
-  populated_matrix[0].map.with_index { |row, i| row.map.with_index { |value, j| 
-
-    if populated_matrix[0][i + 2] != nil && (i + 2) < populated_matrix[0].length && (j + 1) < 8
-      value.adjacent.push(populated_matrix[0][i + 2][j + 1]) 
-    end
-
-    if populated_matrix[0][i + 1] != nil && (i + 1) < 8 && (j + 2) < 8
-      value.adjacent.push(populated_matrix[0][i + 1][j + 2]) 
-    end
-
-    if populated_matrix[0][i + 2] != nil && (i + 2) < 8 && (j - 1) > -1
-      value.adjacent.push(populated_matrix[0][i + 2][j - 1]) 
-    end
-
-    if populated_matrix[0][i + 1] != nil && (i + 1) < 8 && (j - 2) > -1
-      value.adjacent.push(populated_matrix[0][i + 1][j - 2]) 
-    end
-
-    if populated_matrix[0][i - 2][j - 1] && (i - 2) > -1 && (j - 1) > -1
-      value.adjacent.push(populated_matrix[0][i - 2][j - 1]) 
-    end
-
-    if populated_matrix[0][i - 1][j - 2] && (i - 1) > -1 && (j - 2) > -1
-      value.adjacent.push(populated_matrix[0][i - 1][j - 2]) 
-    end
-
-    if populated_matrix[0][i - 2][j + 1] && (i - 2) > - 1 && (j + 1) < 8
-      value.adjacent.push(populated_matrix[0][i - 2][j + 1]) 
-    end
-
-    if populated_matrix[0][i - 1][j + 2] && (i - 1) > -1 && (j + 2) < 8
-      value.adjacent.push(populated_matrix[0][i - 1][j + 2]) 
-    end
-    
-      } }
-
-    return populated_matrix
-
-end  
 
 class Knight 
   attr_accessor :location, :checked, :parent
@@ -112,8 +52,8 @@ def knight_moves
 
   x_start = 0
   y_start = 0
-  x_end = 4
-  y_end = 0
+  x_end = 7
+  y_end = 7
 
   board = map_board
 
@@ -131,26 +71,32 @@ def knight_moves
     knight.location = node_1
 
     while knight.location != target do
-      knight.location.adjacent.map { |node| queue.push(node) }
+      knight.location.adjacent.map { |node| unless queue.include?(node) 
+      queue.push(node) 
+      end}
+      knight.checked.push(queue[0])
       knight.location = queue.shift
     end
 
-    def trace_path (node)
+    return knight.checked.map { |value| value.data }
 
-      count = 0
-      path = []
-      until node.parent === nil
-      node = node.parent
-      end
-      path
-      
-    end  
-      
-    
-    
- 
   end  
 
+=begin  
+  def trace_path (node)
+
+    count = 0
+    path = []
+    until node.parent === nil
+    node = node.parent
+    count += 1
+    path.push(node)
+    end
+
+    return "you made it in #{count} moves - your path is #{path}"
+      
+  end  
+=end    
 
   puts traverse(vertex_1, vertex_2)
   #display each vertex from first location to second location
@@ -159,41 +105,4 @@ end
 
 
 knight_moves 
-
-
-
-
-=begin
-
-while knight.queue.length > 0 do
-
-    puts "loop"
-      knight.queue.map { |value| if value != "level marker" 
-      puts value.data 
-    end}
-
-      if knight.queue[0] === "level marker"
-        puts "block 1"
-        count += 1
-        knight.queue.shift
-      elsif knight.queue[0] === target
-        puts "block 2"
-        knight.path.push(node_2.data)
-        knight.path.map { |space| 
-          puts space }   
-        return  
-      else
-        puts "block 3"
-        #knight.queue[0].parent = knight.location
-        knight.queue[0].adjacent.map { |vertex| if !knight.checked.include?(vertex) 
-        knight.queue.push(vertex) 
-      end}
-        knight.queue.shift
-        knight.location = knight.queue[0]
-      end  
-
-    end  
-
-=end
-
 
